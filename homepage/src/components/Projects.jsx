426e259aa92366ebe19ffa99c1ce3b4c7bedf89b
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-10-02 19:31:36 +0200

    feat: implemented project component

---
+++ homepage/src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+        <div>
+          <h1 className='text-4xl font-bold'>Our Projects</h1>
+          <div className="divider w-2/6"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
