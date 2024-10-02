6d9a68d0a41db14565f94b1be7f09d77c9e3bd13
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-10-02 19:32:13 +0200

    chore: changed font size

--- homepage/src/components/Features.jsx
+++ homepage/src/components/Features.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div className='flex flex-col items-start'>
+    <div className='flex flex-col items-start pt-20'>
         <h2 className="text-4xl font-bold mb-4">👋 Features</h2>
         <div className="divider w-2/6"></div>
-        <p className='pl-6 w-5/12 text-left text-lg text-white'>
+        <p className='pl-6 w-5/12 text-left text-xl text-white'>
         <span className='font-bold'>Welcome to Coding Soul</span> - a vibrant Discord community for programmers of all levels! Whether you're a beginner or expert, you'll find support, collaboration, and creativity here. With channels for various programming languages, frameworks, and tools—like Java, Rust, web development, and machine learning—you'll always have something to explore. Our community shares resources, solves problems, and engages in discussions. Join us for live coding, giveaways, and hackathons, and connect with passionate coders ready to grow together!</p>
     </div>
   )
