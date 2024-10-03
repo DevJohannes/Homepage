2937c08de5671b409e18629a25b5d52d8aa3befd
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-10-03 12:51:40 +0200

    feat: added button on mobile devices

--- homepage/src/components/Heading.jsx
+++ homepage/src/components/Heading.jsx
@@ -5,6 +5,8 @@ const Heading = () => {
     <div className='flex flex-col items-center justify-center h-screen mb-72 '>
         <h1 className='text-6xl text-white font-extrabold'>Coding Soul</h1>
         <p className='pt-5 text-gray-300 text-lg font-bold'>Where Passion Meets Code</p>
+        <a href='https://discord.gg/tApBS2QNMj'><button className="btn btn-active btn-link lg:hidden text-xl">Join Now</button></a>
+
     </div>
   )
 }
