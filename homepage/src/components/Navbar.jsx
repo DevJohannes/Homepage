200a19655afdfcbca87784b2af9e564c4c0ab62d
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-10-03 13:03:35 +0200

    chore: hiding navbar at mobile device

--- homepage/src/components/Navbar.jsx
+++ homepage/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
         <div className="flex-1">
-            <a className="btn btn-ghost text-xl text-white font-bold">Coding Soul</a>
+            <a className="btn btn-ghost text-4xl text-white font-bold min-[320px]:hidden md:block">Coding Soul</a>
         </div>
         <div className="flex-none text-white font-semibold min-[320px]:hidden md:block">
             <ul className="menu menu-horizontal px-1">
